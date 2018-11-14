52e9c915b91b0b2f45859f0f3bfc9c8699a3ec90
olegsochuk <oleg.sobchuk@gmail.com>
2018-11-14 22:47:14 +0200

    Add Create Source

--- controllers/sources.go
+++ controllers/sources.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/olegsobchuk/go-health/configs"
 	"github.com/olegsobchuk/go-health/models"
+	validator "gopkg.in/go-playground/validator.v8"
 )
 
 // IndexSources shows list of existing sources
@@ -16,4 +20,34 @@ func IndexSources(c *gin.Context) {
 		"sources":     sources,
 		"currentUser": user,
 	})
+}
+
+// NewSource render form for New Source
+func NewSource(c *gin.Context) {
+	source := new(models.Source)
+	c.HTML(200, "newSource", gin.H{
+		"title":       "New Source",
+		"source":      source,
+		"currentUser": currentUser,
+	})
+}
+
+// CreateSource creates Source
+func CreateSource(c *gin.Context) {
+	fmt.Printf("%#v\n", c.PostForm("status"))
+	source := new(models.Source)
+	if err := c.ShouldBind(source); err != nil {
+		c.HTML(http.StatusUnauthorized, "newSource", gin.H{
+			"title":  "New Source",
+			"errors": err.(validator.ValidationErrors),
+			"source": source,
+		})
+	} else {
+		source.UserID = currentUserID(c)
+		_, err := source.Create()
+		if err != nil {
+			// set message *something come up* and redirect
+		}
+		c.Redirect(301, "/sources/index")
+	}
 }
--- models/source.go
+++ models/source.go
@@ -1,16 +1,30 @@
 // Package models Source
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/olegsobchuk/go-health/configs"
+)
 
 // SourcesPerPage limit of Sources pagination
 const SourcesPerPage = 30
 
 // Source source struct
 type Source struct {
-	gorm.Model
-	URL    string `form:"url" json:"url" xml:"url" binding:"url,required"`
-	Status bool   `form:"status" json:"status" xml:"status"`
-	UserID int    `form:"user_id" json:"user_id" xml:"user_id"`
-	user   User
+	ID        uint
+	URL       string `form:"url" json:"url" xml:"url" binding:"url,required"`
+	Status    bool   `form:"status" json:"status" xml:"status" binding:"exists"`
+	UserID    uint   `form:"user_id" json:"user_id" xml:"user_id"`
+	user      User
+	CreatedAt time.Time  `form:"-"`
+	UpdatedAt time.Time  `form:"-"`
+	DeletedAt *time.Time `form:"-"`
+}
+
+// Create save instance to DB
+func (source *Source) Create() (*gorm.DB, error) {
+	result := configs.DB.Create(source)
+	return result, nil
 }
