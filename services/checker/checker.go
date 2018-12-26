a520ad99077f6c3b53a91d50ac235b4534063ff7
olegsochuk <oleg.sobchuk@gmail.com>
2018-12-26 22:38:38 +0200

    Add Checker

---
+++ services/checker/checker.go
@@ -0,0 +1,20 @@
+package checker
+
+import (
+	"net/http"
+	"time"
+)
+
+// Check checks if source alive
+func Check(url string) bool {
+	timeout := time.Duration(10 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	resp, err := client.Get(url)
+	if err != nil || resp.StatusCode == 502 {
+		return false
+	} else {
+		return true
+	}
+}
